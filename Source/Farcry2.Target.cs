99326fe99a0087c6d5a2bb766d112e92117ab3a2
Iker Noya <ikernoya4@gmail.com>
2022-04-23 20:59:09 -0300

    Created UE5 project

---
+++ Source/Farcry2.Target.cs
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class Farcry2Target : TargetRules
+{
+	public Farcry2Target(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.Add("Farcry2");
+	}
+}
---
+++ Source/Farcry2Editor.Target.cs
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class Farcry2EditorTarget : TargetRules
+{
+	public Farcry2EditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.Add("Farcry2");
+	}
+}
