6cee512e27e3de93ca68e4e63eab5a7c1316436d
Iker Noya <ikernoya4@gmail.com>
2022-07-21 11:11:40 -0300

    Created grid for inventory

--- Source/Farcry2/Components/InventoryComponent.h
+++ Source/Farcry2/Components/InventoryComponent.h
@@ -4,20 +4,33 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Inventory/Items/ItemData.h"
 #include "InventoryComponent.generated.h"
 
 USTRUCT(BlueprintType)
 struct FSlot
 {
 	GENERATED_BODY()
-	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UItemData* Item = nullptr;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	bool bIsUsed = false;
 };
 
-UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent), Blueprintable)
 class FARCRY2_API UInventoryComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inventory")
+	TArray<FSlot> Slots;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inventory")
+	int32 Columns = 8;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inventory")
+	int32 Rows = 8;
+	
 public:
 	// Sets default values for this component's properties
 	UInventoryComponent();
@@ -30,4 +43,9 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
 	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	FORCEINLINE int32  GetColumns() const {return Columns;}
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	FORCEINLINE int32 GetRows() const {return Rows;}
 };
